b790da8dcaa538ef2c0427c9c63f8dd7739add24
“张桂福” <904824669@qq.com>
2020-08-13 23:36:21 +0800

    发布评论、评论回复

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -13,7 +13,7 @@ class EnableCrossRequestMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-            $response->header('Access-Control-Allow-Origin', 'localhost:8080');
+            $response->header('Access-Control-Allow-Origin', '*');
             $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
             $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
 
