90bfce039422694ba19cf0f629ec96eda960ab5b
“张桂福” <904824669@qq.com>
2020-08-26 12:04:12 +0800

    补充jwxt必要表

---
+++ database/migrations/2020_08_26_115756_update_time.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTime extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('update_time', function (Blueprint $table){
+            $table->id();
+
+            $table->dateTime('student_id')->comment('学号');
+            $table->dateTime('info')->comment('个人信息');
+            $table->dateTime('grade')->comment('成绩');
+            $table->dateTime('schedule')->comment('考试安排');
+            $table->dateTime('allschedule')->comment('所有安排');
+            $table->dateTime('exam')->comment('考试');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('update_time');
+    }
+}
---
+++ database/migrations/2020_08_26_115807_flag.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Flag extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flag', function (Blueprint $table){
+            $table->id();
+
+            $table->string('student_id')->comment('学号');
+            $table->string('info')->comment('个人信息');
+            $table->string('grade')->comment('成绩');
+            $table->string('schedule')->comment('考试安排');
+            $table->string('allschedule')->comment('所有安排');
+            $table->string('exam')->comment('考试');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flag');
+    }
+}
