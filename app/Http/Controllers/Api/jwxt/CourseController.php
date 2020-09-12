397a4103edcd9e7ddcc4a6179bc2af9d2b918392
“张桂福” <904824669@qq.com>
2020-09-12 13:41:05 +0800

    swaggerui bug

--- app/Http/Controllers/Api/jwxt/CourseController.php
+++ app/Http/Controllers/Api/jwxt/CourseController.php
@@ -156,4 +156,105 @@ class CourseController extends Controller
         }
         return json_encode(array("code" => 0, "message" => "查询成功","data" => $result), JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     }
+    //空课表导出excel函数
+    private function TableExcel($data)
+    {
+        $result = array();
+        $stuNum = 0;
+        $data = json_decode($data,true);
+        foreach ($data as $stu)
+        {
+            $schedule = $stu['schedule'];
+            $name = $stu['name'];
+            $table = array();
+            $table['name'] = $name;
+            for($i = 1; $i < 8; $i++)
+            {
+                for($j = 1; $j < 12; $j++)
+                {
+                    $table[$i][$j] = 1;
+                }
+            }
+            foreach ($schedule as $keyS => $data)
+            {
+                for($i = 1; $i < 8; $i++)
+                {
+                    if(!empty($data[$i]))
+                    {
+                        $woGan = $data[$i];
+                        foreach ($woGan as $woc) {
+                            # code...if(!empty($data[$i][$j]))
+                            {
+
+                                $val = $woc;
+                                while($val['section_length']--)
+                                {
+                                    $table[$i][$val['section_start']++] = 0;
+                                }
+                            }
+                        }
+                        for($j = 1; $j < 6; $j++)
+                        {
+
+                            if(!empty($data[$i][$j]))
+                            {
+
+                                $val = $data[$i][$j];
+
+                                while($val['section_length']--)
+                                {
+                                    $table[$i][$val['section_start']++] = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            $result[$stuNum++] = $table;
+        }
+        $timeTableWoGan = $result;
+        $tableWoGan = "<html xmlns:x='urn:schemas-microsoft-com:office:excel'><body>";
+        $tableWoGan = $tableWoGan.'<table border="1">';
+        $tableWoGan = $tableWoGan.'<tr>
+                    <th> </th>
+                    <th>周一</th>
+                    <th>周二</th>
+                    <th>周三</th>
+                    <th>周四</th>
+                    <th>周五</th>
+                    <th>周日</th>
+                    <th>周六</th>
+                </tr>';
+        $i = 0;
+// echo $tableWoGan;
+        for ($i=1; $i < 12; $i++)//循环生成表格的后面十一行
+        {
+            $tableWoGan = $tableWoGan.  '<tr>';
+            # code...
+            //生成每一行的八个格子
+            $j = 0;
+            $tableWoGan = $tableWoGan."<td>第$i"."节</td>";
+            for ($j=1; $j < 8; $j++)
+            {
+                # code...
+                $tableWoGan = $tableWoGan."<td>";
+                //将每个人的名字丢到格子里面
+                foreach ($timeTableWoGan as $time) {
+                    # code...
+                    if($time[$j][$i] == 1)
+                    {
+                        $tableWoGan = $tableWoGan.$time['name']."<br>";
+                    }
+                }
+                $tableWoGan = $tableWoGan."</td>";
+            }
+            $tableWoGan = $tableWoGan.  '</tr>';
+        }
+        $tableWoGan = $tableWoGan.   '</table></body></html>';
+        $rand = mt_rand(100,9999);
+        $rand = (string)$rand;
+        $filename = $rand.time()."kkb.excel";
+        file_put_contents($filename, $tableWoGan);
+
+    }
 }
