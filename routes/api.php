92da1d211241c11d8395fb3a914d1b869ca11826
“张桂福” <904824669@qq.com>
2021-03-12 23:06:12 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,11 @@ Route::namespace('Api')->group(function (){
 
     /** 用户区 */
     Route::group(['middleware' => 'login.check'], function () {
+        /** 关注 */
+
+        Route::post('focus',"Eatest\FocusController@focus")->middleware(['focus.exist.check']);
+        Route::post('unfocus',"Eatest\FocusController@unfocus")->middleware(['unfocus.exist.check']);
+        Route::get('focus/{uid}',"Eatest\FocusController@get_user_focus_list");
         /** 推送 */
         Route::post('push/toSingle',"PushSdk\ToSingleController@pushMessage");
         /** Tip */
