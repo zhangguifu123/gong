5a4d814ca2959a40059d390e685fe34a766f9404
“张桂福” <904824669@qq.com>
2021-03-12 22:56:19 +0800

    登录

--- app/Http/Controllers/Api/StudentLoginController.php
+++ app/Http/Controllers/Api/StudentLoginController.php
@@ -48,6 +48,8 @@ class StudentLoginController extends Controller
                     'like'       => '[]',
                     'eatest'     => '[]', //mysql 中 json 默认值只能设置为NULL 为了避免不必要的麻烦，在创建的时候赋予初始值
                     'countdown'  => '[]',
+                    'focus'      => 0,
+                    'focused'    => 0,
                     'collection' => '[]',
                     'remember'   => md5($data['password'] . time() . rand(1000, 2000))
                 ]);
