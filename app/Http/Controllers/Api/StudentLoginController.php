a5608cde83e826eed8494881c138bd4004d4173c
“张桂福” <904824669@qq.com>
2021-04-06 23:10:04 +0800

    header jwt 4

--- app/Http/Controllers/Api/StudentLoginController.php
+++ app/Http/Controllers/Api/StudentLoginController.php
@@ -8,6 +8,7 @@ use App\Model\Eatest\Evaluation;
 use App\Model\User\Ecard;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTFactory;
