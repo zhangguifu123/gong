41fffdf637a6ed45c9434530a8a0ffa303aad74e
周旭光 <942028519@qq.com>
2021-04-14 20:11:27 +0800

    中间件

--- app/Http/Middleware/Course/GroupMemberCheck.php
+++ app/Http/Middleware/Course/GroupMemberCheck.php
@@ -18,12 +18,12 @@ class GroupMemberCheck
     public function handle($request, Closure $next)
     {
         $stu_id = JWTAuth::parseToken()->authenticate()->stu_id;
-        var_dump($stu_id . PHP_EOL);
-        var_dump($request->route('sharingCode'));
+//        var_dump($stu_id . PHP_EOL);
+//        var_dump($request->route('sharingCode'));
         $course = CourseGroup::query()->where('sharingCode',$request->route('sharingCode'))->first();
 //            ->get('member')->toArray();
-        var_dump($course->member . PHP_EOL);
-        var_dump(json_decode($course->member, true) . PHP_EOL);
+//        var_dump($course->member . PHP_EOL);
+//        var_dump(json_decode($course->member, true) . PHP_EOL);
 //        return response("失败");
         if( in_array($stu_id, json_decode($course->member, true))) {
             return response(msg(3, "用户已存在" . __LINE__));
