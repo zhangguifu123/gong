de14122b73a56da34ba7b04787d183bbb64d7e26
周旭光 <942028519@qq.com>
2021-04-21 22:54:09 +0800

    管理员验证

--- app/Http/Controllers/Api/Eatest/searchEatestController.php
+++ app/Http/Controllers/Api/Eatest/searchEatestController.php
@@ -9,23 +9,44 @@ use Illuminate\Http\Request;
 
 class searchEatestController extends Controller
 {
+
+
+    //排序
+    /**
+     * 标题、内容、标签、话题
+     * 标题、内容、标签
+     * 标题、内容
+     * 标题
+     *
+     * 内容、标签、话题
+     * 内容、标签
+     * 内容
+     *
+     * 标签、话题
+     * 标签
+     *
+     * 话题
+     */
+
+
+    //        if('标题中含有'){                 //搜索标题
+//            $this->titleSearch();
+//        }else if('标题中不含,标签和内容中含有'){     //搜索标签和内容
+//            $this->LocSearch();
+//        }else if('标题、标签、内容中不含，话题中含有'){                       //搜索话题
+//            $this->topicSearch();
+//        }else{
+//            return msg(11, __LINE__);
+//        }
+
+
     //模糊搜索
     public function search(Request $request)
     {
-        //检查数据格式
-//        $params = [
-//            'index' => ['string']
-//        ];
-////        $request = handleData($request,$params);
-//        if(!is_object($request)){
-//            return $request;
-//        }
         //获取数据
         $index = $request->route('index');
         $page = $request->route('page');
-//        return $page;
         $offset = $page * 8 - 8;
-//        var_dump($index);
         //拉取所有符合条件的评测
         $evaluation_list = Evaluation::query()
             ->limit(8)
@@ -42,40 +63,48 @@ class searchEatestController extends Controller
                 "collections", "top", "img", "title", "users.avatar", "evaluations.created_at as time"
             ])
             ->toArray();
-//        $list = Evaluation::all();
-//        return $list;
         if (!$evaluation_list) {
             return msg(4, __LINE__);
         }
         $message = $this->isLike_Collection($request, $evaluation_list);
         return msg(0, $message);
     }
-        //排序
-        /**
-         * 标题、内容、标签、话题
-         * 标题、内容、标签
-         * 标题、内容
-         * 标题
-         *
-         * 内容、标签、话题
-         * 内容、标签
-         * 内容
-         *
-         * 标签、话题
-         * 标签
-         *
-         * 话题
-         */
 
-//        if('标题中含有'){                 //搜索标题
-//            $this->titleSearch();
-//        }else if('标题中不含,标签和内容中含有'){     //搜索标签和内容
-//            $this->LocSearch();
-//        }else if('标题、标签、内容中不含，话题中含有'){                       //搜索话题
-//            $this->topicSearch();
-//        }else{
-//            return msg(11, __LINE__);
-//        }
+
+    //条件搜索
+    public function cdSearch(Request $request)
+    {
+        //获取数据
+        $index = $request->route('index');
+        $topic = $request->route('topic');
+        $orderBy = $request->route('orderBy');
+        $page = $request->route('page');
+        $offset = $page * 8 - 8;
+        //拉取所有符合条件的评测
+        $evaluation_list = Evaluation::query()
+            ->limit(8)
+            ->offset($offset)
+            ->orderByDesc("evaluations.created_at")
+            ->where('title', 'like', '%' . $index . '%')      //标题
+            ->orwhere('content', 'like', '%' . $index . '%')      //内容
+            ->orWhere('label', 'like', '%' . $index . '%')      //标签
+            ->orWhere('topic', 'like', '%' . $index . '%')       //话题
+            ->whereIn('evaluations.status', [0, 1])
+            ->leftJoin('users', 'evaluations.publisher', '=', 'users.id')
+            ->get([
+                "evaluations.id", "users.nickname as publisher_name", "label", "topic", "views", "evaluations.like",
+                "collections", "top", "img", "title", "users.avatar", "evaluations.created_at as time"
+            ])
+            ->toArray();
+        if (!$evaluation_list) {
+            return msg(4, __LINE__);
+        }
+        $message = $this->isLike_Collection($request, $evaluation_list);
+        return msg(0, $message);
+    }
+
+
+
 
 
 
