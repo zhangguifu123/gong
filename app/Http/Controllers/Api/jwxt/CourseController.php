925100e536f15617add822eddd673819cf4c75a2
周旭光 <942028519@qq.com>
2021-04-27 14:39:07 +0800

    空课表

--- app/Http/Controllers/Api/jwxt/CourseController.php
+++ app/Http/Controllers/Api/jwxt/CourseController.php
@@ -296,10 +296,13 @@ class CourseController extends Controller
         //获取有课成员
 
         foreach ($uids as $uid) {
-            $course = Http::get('https://gong.sky31.com/api/course/extra/' . $uid);
+            $course = Http::get('https://jwxt.sky31.com/api/student/'.$uid.'/course');
             $course_list = json_decode($course->body(),true)['data'];
+            return $course_list;
+            return gettype($course_list);
             if (!is_object($course_list)) {
-                $course_list = [];
+                return msg(5, '课表初始化失败,请重新登陆');
+//                $course_list = [];
             }
 //            return $course_list;
             foreach ($course_list as $i => $item) {
