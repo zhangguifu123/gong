d428fc8f82ef8a4e66c98a0c656cb84bd3688c3f
周旭光 <942028519@qq.com>
2021-06-05 20:29:45 +0800

    eatest列表

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+
 Route::namespace('Api')->group(function (){
 
     /** 公共区 */
